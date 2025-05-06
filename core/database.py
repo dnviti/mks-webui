bd2bc33177f584cdff5f73043d4dae64eb7abaf9
Daniele Viti <dnviti@gmail.com>
2025-05-07 01:10:38 +0200

    database into data folder

--- core/database.py
+++ core/database.py
@@ -1,7 +1,12 @@
 from sqlalchemy import create_engine
+import os
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
-DATABASE_URL = "sqlite:///./printers.db"          # ← local file, no server needed
+DATABASE_URL = "sqlite:///./data/printers.db"          # ← local file, no server needed
+newpath = r'./data'                 # ← path to the database file
+if not os.path.exists(newpath):
+    os.makedirs(newpath)
+
 engine = create_engine(
     DATABASE_URL, connect_args={"check_same_thread": False}
 )
